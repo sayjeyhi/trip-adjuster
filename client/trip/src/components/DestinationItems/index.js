8488382c9b20440e52b1df02e029f21668299b58
mahsa.mesbah <mahsa.mesbah@snapp.market>
2022-03-02 00:52:47 +0330

    add swipper in destinationsItems component

--- client/trip/src/components/DestinationItems/index.js
+++ client/trip/src/components/DestinationItems/index.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import { Image, Typography, Anchor } from 'antd';
-import { LocationCurvedIcon, StarCurvedIcon } from '@iconbox/iconly'; 
+import { Typography, Anchor } from 'antd';
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+
+import "swiper/css";
+import "swiper/css/pagination";
 
 import DestinationCard from '../DestinationCard';
-import des3 from '../../assets/images/Destination-3.jpeg';
 
 import { StyledDestinationItemsWrapper, 
        } from './style';
 
 const DestinationItems = () => {
 
-    const { Text, Title } = Typography;
-    const { Link } = Anchor;
-
     return (
       <StyledDestinationItemsWrapper>
-        <DestinationCard />
-        <DestinationCard />
+        <Swiper slidesPerView={1.5} >
+          <SwiperSlide>
+            <DestinationCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <DestinationCard />
+          </SwiperSlide>
+      </Swiper>
       </StyledDestinationItemsWrapper>
     );
 }

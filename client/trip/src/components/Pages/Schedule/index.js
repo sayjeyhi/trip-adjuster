35ebeae4501aa848a75859257aaddb5aa37483ba
mahsa.mesbah <mahsa.mesbah@snapp.market>
2022-03-12 13:54:05 +0330

    fix format of files

--- client/trip/src/components/Pages/Schedule/index.js
+++ client/trip/src/components/Pages/Schedule/index.js
@@ -4,22 +4,26 @@ import { useNavigate } from 'react-router-dom';
 import { Row, Col } from 'antd';
 import DatePicker from 'sassy-datepicker';
 
-import { CalendarLightIcon } from '@iconbox/iconly'; 
-import { TicketIcon } from '@iconbox/jamicons'; 
+import { CalendarLightIcon } from '@iconbox/iconly';
+import { TicketIcon } from '@iconbox/jamicons';
 
 import Header from '../../Common/Header';
 import CardHeader from '../../Common/CardHeader';
 import RecommendedCard from '../Recommended/partials/RecommendedCard';
-import Footer from "../../Common/Footer";
+import Footer from '../../Common/Footer';
 
 import des4 from '../../../assets/images/Destination-4.jpeg';
 import des5 from '../../../assets/images/Destination-5.jpeg';
 import des6 from '../../../assets/images/Destination-6.jpeg';
 
-import { StyledScheduleWrapper, StyledDatePickerWrapper, StyledRecommentCardWrapper, StyledIconWrapper } from './style';
+import {
+  StyledScheduleWrapper,
+  StyledDatePickerWrapper,
+  StyledRecommentCardWrapper,
+  StyledIconWrapper,
+} from './style';
 
 const Schedule = () => {
-
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -27,17 +31,17 @@ const Schedule = () => {
     const loginStatus = JSON.parse(loggedIn);
     if (!loginStatus) {
       navigate('/login');
-    } 
-  },[])
+    }
+  }, []);
 
   const onChange = (date) => {
     console.log(date.toString());
   };
 
-    return (
+  return (
     <>
       <Row className="ant-row ant-row-center">
-        <Col xl={8} lg={10} md={15} >
+        <Col xl={8} lg={10} md={15}>
           <StyledScheduleWrapper>
             <Header title="Schedule" type="Schedule" />
             <StyledDatePickerWrapper>
@@ -45,19 +49,73 @@ const Schedule = () => {
             </StyledDatePickerWrapper>
             <CardHeader title="My Schedule" link="/recommended" />
             <StyledRecommentCardWrapper>
-              <RecommendedCard image={des4} icon={<StyledIconWrapper><TicketIcon size={2.5} /></StyledIconWrapper>} title="Pantai Nusa Penida" text={[<CalendarLightIcon />, "22 jun 2022"]} />
-              <RecommendedCard image={des5} icon={<StyledIconWrapper><TicketIcon size={2.5} /></StyledIconWrapper>} title="Pantai Tanah Lot" text={[<CalendarLightIcon />, "22 jun 2022"]} />
-              <RecommendedCard image={des6} icon={<StyledIconWrapper><TicketIcon size={2.5} /></StyledIconWrapper>} title="Pantai Nusa Dua" text={[<CalendarLightIcon />, "22 jun 2022"]} />
-              <RecommendedCard image={des4} icon={<StyledIconWrapper><TicketIcon size={2.5} /></StyledIconWrapper>} title="Pantai Nusa Penida" text={[<CalendarLightIcon />, "22 jun 2022"]} />
-              <RecommendedCard image={des5} icon={<StyledIconWrapper><TicketIcon size={2.5} /></StyledIconWrapper>} title="Pantai Tanah Lot" text={[<CalendarLightIcon />, "22 jun 2022"]} />
-              <RecommendedCard image={des6} icon={<StyledIconWrapper><TicketIcon size={2.5} /></StyledIconWrapper>} title="Pantai Nusa Dua" text={[<CalendarLightIcon />, "22 jun 2022"]} />
+              <RecommendedCard
+                image={des4}
+                icon={
+                  <StyledIconWrapper>
+                    <TicketIcon size={2.5} />
+                  </StyledIconWrapper>
+                }
+                title="Pantai Nusa Penida"
+                text={[<CalendarLightIcon key="1" />, '22 jun 2022']}
+              />
+              <RecommendedCard
+                image={des5}
+                icon={
+                  <StyledIconWrapper>
+                    <TicketIcon size={2.5} />
+                  </StyledIconWrapper>
+                }
+                title="Pantai Tanah Lot"
+                text={[<CalendarLightIcon key="2" />, '22 jun 2022']}
+              />
+              <RecommendedCard
+                image={des6}
+                icon={
+                  <StyledIconWrapper>
+                    <TicketIcon size={2.5} />
+                  </StyledIconWrapper>
+                }
+                title="Pantai Nusa Dua"
+                text={[<CalendarLightIcon key="3" />, '22 jun 2022']}
+              />
+              <RecommendedCard
+                image={des4}
+                icon={
+                  <StyledIconWrapper>
+                    <TicketIcon size={2.5} />
+                  </StyledIconWrapper>
+                }
+                title="Pantai Nusa Penida"
+                text={[<CalendarLightIcon key="4" />, '22 jun 2022']}
+              />
+              <RecommendedCard
+                image={des5}
+                icon={
+                  <StyledIconWrapper>
+                    <TicketIcon size={2.5} />
+                  </StyledIconWrapper>
+                }
+                title="Pantai Tanah Lot"
+                text={[<CalendarLightIcon key="5" />, '22 jun 2022']}
+              />
+              <RecommendedCard
+                image={des6}
+                icon={
+                  <StyledIconWrapper>
+                    <TicketIcon size={2.5} />
+                  </StyledIconWrapper>
+                }
+                title="Pantai Nusa Dua"
+                text={[<CalendarLightIcon key="6" />, '22 jun 2022']}
+              />
             </StyledRecommentCardWrapper>
             <Footer title="schedule" />
           </StyledScheduleWrapper>
         </Col>
       </Row>
     </>
-    );
-}
+  );
+};
 
 export default Schedule;
--- client/trip/src/components/queries/index.js
+++ client/trip/src/components/queries/index.js
@@ -1,110 +1,97 @@
-import {
-    ApolloClient,
-    InMemoryCache,
-    createHttpLink,
-    gql
-  } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink, gql } from '@apollo/client';
 
-  import { setContext } from '@apollo/client/link/context';
+import { setContext } from '@apollo/client/link/context';
 
-  import {offsetLimitPagination} from '@apollo/client/utilities';  
+import { offsetLimitPagination } from '@apollo/client/utilities';
 
-  const httpLink = createHttpLink({
-    uri: 'http://localhost:4001/graphql',
-  });  
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4001/graphql',
+});
 
-  const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem('token');
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? token : "",
-      }
-    }
-  });
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? token : '',
+    },
+  };
+});
 
 export const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache({
-      typePolicies: {
-        Query: {
-          fields: {
-            discounts: offsetLimitPagination()
-          }
-        }
-      }
-    })
-  });
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          discounts: offsetLimitPagination(),
+        },
+      },
+    },
+  }),
+});
 
-  export const getDiscountsQuery = gql`
+export const getDiscountsQuery = gql`
   query Discounts($offset: Int!, $limit: Int!) {
-    discounts(
-      offset: $offset
-      limit: $limit
-      sort: "launch_date_utc"
-      order: "desc"
-    ) {
-        id
-        code
-        active
-        discount
-        validUntil
-        locationTitle
+    discounts(offset: $offset, limit: $limit, sort: "launch_date_utc", order: "desc") {
+      id
+      code
+      active
+      discount
+      validUntil
+      locationTitle
     }
   }
 `;
 
-
 export const getDestinationsQuery = gql`
-{
-    destinations{
-        id
-        name
-        description
-        star
-        price
-        destination
-        city
-        country
+  {
+    destinations {
+      id
+      name
+      description
+      star
+      price
+      destination
+      city
+      country
     }
-}
+  }
 `;
 
 export const getDestinationQuery = gql`
-
-    query Destination($id: ID!) {
-        destination(id: $id){
-            id
-            name
-            description
-            star
-            price
-            destination
-            city
-            country
-        }
+  query Destination($id: ID!) {
+    destination(id: $id) {
+      id
+      name
+      description
+      star
+      price
+      destination
+      city
+      country
     }
-
+  }
 `;
 
 export const getNotificationsQuery = gql`
-{
-    notifications{
-        id
-        title
-        time
-        userId
+  {
+    notifications {
+      id
+      title
+      time
+      userId
     }
-}
+  }
 `;
 
 export const getLoginMutation = gql`
-    mutation Login($username: String!, $password: String! ) {
-        login(username: $username, password: $password) {
-            name
-            token
-            id
-            loggedIn
-        }
+  mutation Login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      name
+      token
+      id
+      loggedIn
     }
+  }
 `;
